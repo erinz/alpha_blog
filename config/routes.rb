ed9eb76a1fc19e7903b1f0dee416a90e9e22d72f
erinz <erinuw@gmail.com>
2015-12-20 15:20:01 -0800

    change pages#home to root route

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'pages/home' => 'pages#home'
-  get 'pages/about' => 'pages#about'
+  root 'pages#home'
+  get 'about' => 'pages#about'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
