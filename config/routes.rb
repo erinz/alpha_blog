77f9174de1bd72db81c056b0665fcd8ea60b165f
erinz <uwerin@gmail.com>
2015-12-25 21:03:58 -0800

    add user login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+
   resources :users, except: [:new]
   resources :articles
   # Example of named route that can be invoked with purchase_url(id: product.id)
